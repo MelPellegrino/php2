666dac7d1320767188e536f6cf7194e63798ba37
MelanieP <melpellegrino@gmail.com>
2014-06-04 12:25:57 -0700

    Added test.txt

---
+++ week1/contact-form-handler.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+
+<body>
+
+<?php  
+
+echo "user name is".$_GET['name']; // reaching for value of name input
+echo "<br/>";
+
+echo "user age is".$_GET['age']; // reaching for value of age input
+echo "<br/>";
+
+echo "user address is".$_GET['address']; // reaching for value of address input
+echo "<br/>";
+
+?>
+
+
+</body>
+</html>
---
+++ week1/contact-form.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+
+<body>
+	<form action="contact-form-handler.php" method="GET">
+		Name <input name="name" type="text" /> <br/>
+		Age <input name="age" type="text" /> <br/>
+		Address <input name="address" type="text" /> <br/>
+		<input type="submit" value="Send" />
+	</form>
+</body>
+</html>
+
